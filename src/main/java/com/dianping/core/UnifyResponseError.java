16a7709e9d999117ddf43063c0c4aa80ad92603a
wanggang <arthurwanggang@yeah.net>
2021-11-04 23:51:31 +0800

    1： 增加一些基本的容错处理

--- src/main/java/com/dianping/core/UnifyResponseError.java
+++ src/main/java/com/dianping/core/UnifyResponseError.java
@@ -1,4 +1,37 @@
 package com.dianping.core;
 
 public class UnifyResponseError {
+
+    //错误码
+    private Integer errCode;
+
+
+    //错误描述
+    private String errMsg;
+
+    public UnifyResponseError(Integer errCode, String errMsg) {
+        this.errCode = errCode;
+        this.errMsg = errMsg;
+    }
+
+    public UnifyResponseError(EmBusinessError emBusinessError){
+        this.errCode = EmBusinessError.getErrCode();
+        this.errMsg = EmBusinessError.getErrMsg();
+    }
+
+    public Integer getErrCode() {
+        return errCode;
+    }
+
+    public void setErrCode(Integer errCode) {
+        this.errCode = errCode;
+    }
+
+    public String getErrMsg() {
+        return errMsg;
+    }
+
+    public void setErrMsg(String errMsg) {
+        this.errMsg = errMsg;
+    }
 }
