d4eb3a0dedc1cc958c4f5ede0ef6b9a25ed731c5
wanggang <arthurwanggang@yeah.net>
2022-01-09 23:30:43 +0800

    1：增加shop的基础建设3

--- src/main/java/com/dianping/service/ShopService.java
+++ src/main/java/com/dianping/service/ShopService.java
@@ -1,13 +1,14 @@
 package com.dianping.service;
 
+import com.dianping.core.BusinessException;
 import com.dianping.model.ShopModel;
 
 import java.util.List;
 
 public interface ShopService {
 
-    ShopModel create(ShopModel shopModel);
-    ShopModel get(Integer id);
+    ShopModel create(ShopModel shopModel) throws BusinessException;
+    ShopModel get(Integer id) throws BusinessException;
     List<ShopModel> selectAll();
 
 }
--- src/main/java/com/dianping/service/impl/ShopServiceImpl.java
+++ src/main/java/com/dianping/service/impl/ShopServiceImpl.java
@@ -1,23 +1,74 @@
 package com.dianping.service.impl;
 
+import com.dianping.core.BusinessException;
+import com.dianping.core.EmBusinessError;
+import com.dianping.dal.ShopModelMapper;
+import com.dianping.model.CategoryModel;
+import com.dianping.model.SellerModel;
 import com.dianping.model.ShopModel;
+import com.dianping.service.CategoryService;
+import com.dianping.service.SellerService;
 import com.dianping.service.ShopService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
+@Service
 public class ShopServiceImpl implements ShopService {
+
+    @Autowired
+    private  ShopModelMapper shopModelMapper;
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @Autowired
+    private SellerService sellerService;
+
     @Override
-    public ShopModel create(ShopModel shopModel) {
-        return null;
+    public ShopModel create(ShopModel shopModel) throws BusinessException {
+        shopModel.setCreatedAt(new Date());
+        shopModel.setUpdatedAt(new Date());
+        // 检查商家商户正确
+        SellerModel sellerModel = sellerService.get(shopModel.getId());
+        if (sellerModel == null) {
+            throw new BusinessException(EmBusinessError.PARAMETER_VALIDATION_ERROR, "商户不存在");
+
+        }
+        // 检查商家是否被禁用
+        int isFlag = sellerModel.getDisableFlag();
+        if (isFlag == 1) {
+            throw new BusinessException(EmBusinessError.PARAMETER_VALIDATION_ERROR, "商户已经禁用");
+        }
+        // 对类目的校验
+        CategoryModel categoryModel = categoryService.get(shopModel.getCategoryId());
+        if (categoryModel == null) {
+            return null;
+        }
+        shopModelMapper.insertSelective(shopModel);
+        return get(shopModel.getId());
     }
 
     @Override
-    public ShopModel get(Integer id) {
-        return null;
+    public ShopModel get(Integer id) throws BusinessException {
+        ShopModel shopModel = shopModelMapper.selectByPrimaryKey(id);
+        if (shopModel == null) {
+            throw new BusinessException(EmBusinessError.PARAMETER_VALIDATION_ERROR, "查询不存在");
+        }
+        shopModel.setCategoryModel(categoryService.get(shopModel.getCategoryId()));
+        shopModel.setSellerModel(sellerService.get(shopModel.getSellerId()));
+        return  shopModel;
     }
 
     @Override
     public List<ShopModel> selectAll() {
-        return null;
+        List<ShopModel> shopModelList = shopModelMapper.selectAll();
+        shopModelList.forEach(shopModel -> {
+            shopModel.setCategoryModel(categoryService.get(shopModel.getCategoryId()));
+            shopModel.setSellerModel(sellerService.get(shopModel.getSellerId()));
+        });
+        return shopModelList;
     }
 }
