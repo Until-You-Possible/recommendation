5f401ee578bcdff3df6e9dc6cb2987fe8f80e4aa
wanggang <arthurwanggang@yeah.net>
2022-01-12 23:25:37 +0800

    1: 对table和form的公用型的思考

---
+++ frontend/my-app/src/components/common/Table.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+// 实现目标
+// 1： table能满足当前业务的公共需求
+// 2： 包括loading 分页(内部做分页) 对外提供刷新操作
+
+
+// 1： column
+// loading状态
+// 对外事件的处理
+
+export default function Table() {
+    return (
+        <div className="table-wrapper">
+            this is common table
+        </div>
+    )
+}
+
---
+++ frontend/my-app/src/components/common/TableForm.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+// 实现目标
+// 与table的联动关系
+// 关于form的不同验证规则
+// 不同控件的数据渲染 eg: select
+// 数据的回填
+
+export default function TableForm() {
+    return (
+        <div>
+            this is table form
+        </div>
+    )
+}
