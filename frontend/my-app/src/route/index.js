0d746e570845d86eb3c1c46969f80d8525d1525e
wanggang <arthurwanggang@yeah.net>
2022-01-12 23:29:52 +0800

    1: 对路由的统一管理

--- frontend/my-app/src/route/index.js
+++ frontend/my-app/src/route/index.js
@@ -1,11 +1,33 @@
 // let routes;
-// export default routes = [
-//     {
-//         path: "/",
-//         name: "home"
-//     },
-//     {
-//         path: "/login",
-//         name: "登陆"
-//     }
-// ]
+export default routes = [
+    {
+        id: 1,
+        path: "/",
+        name: "首页",
+        icon: ""
+    },
+    {
+        id: 2,
+        path: "/login",
+        name: "登陆",
+        icon: ""
+    },
+    {
+        id: 3,
+        path: "/category",
+        name: "品类查询",
+        icon: ""
+    },
+    {
+        id: 4,
+        path: "/shop",
+        name: "门店服务",
+        icon: ""
+    },
+    {
+        id: 5,
+        path: "/seller",
+        name: "商户查询",
+        icon: ""
+    }
+]
